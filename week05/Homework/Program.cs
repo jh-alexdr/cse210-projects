0d487ecdec9442043d4e0602df2325b0b929ac58
jh-alexdr <jtobar@byupathway.edu>
2025-06-05 12:49:54 -0500

    homework

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        // Test Assignment base class
+        Assignment assignment = new Assignment("Jhon Tobar", "Geometry");
+        Console.WriteLine(assignment.GetSummary());
+        Console.WriteLine();
+
+        // Test MathAssignment
+        MathAssignment math = new MathAssignment("Evelin Guillen", "Algebra", "5.2", "12-18, 22");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
+        Console.WriteLine();
+
+        // Test WritingAssignment
+        WritingAssignment writing = new WritingAssignment("Rosa Santamaria", "World Literature", "The Power of Storytelling");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
     }
 }
